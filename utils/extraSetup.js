b83f0734abb0d9afbe4ec17e5f66655afd49759b
Zaidan Noor <zaidannoor06@gmail.com>
2023-06-05 15:49:45 +0700

    Add drop table in migration table message

--- utils/extraSetup.js
+++ utils/extraSetup.js
@@ -1,36 +1,35 @@
 function applyExtraSetup(sequelize) {
-    const { User, Role, Message } = sequelize.models;
-  
-    Role.hasMany(User, {
-      foreignKey: "id_role",
-      targetKey: "id",
-    });
-  
-    User.belongsTo(Role, {
-      foreignKey: "id_role",
-      targetKey: "id",
-    });
-  
-    User.hasMany(Message, {
-      foreignKey: "pengirim",
-      targetKey: "id",
-    });
-  
-    User.hasMany(Message, {
-      foreignKey: "penerima",
-      targetKey: "id",
-    });
-  
-    Message.belongsTo(User, {
-      foreignKey: "pengirim",
-      targetKey: "id",
-    });
-  
-    Message.belongsTo(User, {
-      foreignKey: "penerima",
-      targetKey: "id",
-    });
-  }
-  
-  module.exports = applyExtraSetup;
-  
+  const { User, Role, Message } = sequelize.models;
+
+  Role.hasMany(User, {
+    foreignKey: "id_role",
+    targetKey: "id",
+  });
+
+  User.belongsTo(Role, {
+    foreignKey: "id_role",
+    targetKey: "id",
+  });
+
+  User.hasMany(Message, {
+    foreignKey: "pengirim",
+    targetKey: "id",
+  });
+
+  User.hasMany(Message, {
+    foreignKey: "penerima",
+    targetKey: "id",
+  });
+
+  Message.belongsTo(User, {
+    foreignKey: "pengirim",
+    targetKey: "id",
+  });
+
+  Message.belongsTo(User, {
+    foreignKey: "penerima",
+    targetKey: "id",
+  });
+}
+
+module.exports = applyExtraSetup;
